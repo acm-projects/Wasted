559fc897e8de31d7b2d81f7e654c6dc81a555384
kingsquidicus <sbalaka2002@gmail.com>
2023-10-11 21:16:06 -0500

    User authentication

---
+++ WastedApp/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCVABEM3uRas2WyfL7e8ppDjGFJku_Ses4",
+  authDomain: "wasted-e06e2.firebaseapp.com",
+  projectId: "wasted-e06e2",
+  storageBucket: "wasted-e06e2.appspot.com",
+  messagingSenderId: "411009541808",
+  appId: "1:411009541808:web:8fd43e83b8397b45797d75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+ 
